a3f18b84c0d706ded34ab90a6822cb19668e486e
prakash <p.prakashpaudel35@gmail.com>
2024-12-08 00:45:42 +0530

    the basic custome promise creation task

--- day2Promises/callBacksPromise/customePromise.js
+++ day2Promises/callBacksPromise/customePromise.js
@@ -1,22 +1,40 @@
-
-const completedAssignment = new Promise((res , rej)=>{
-    const isCompleted = true
-    isCompleted ? res("yes the assignment was completed") : rej("the assignment wasnt completed")
-})
+const completedAssignment = new Promise((res, rej) => {
+  const isCompleted = true;
+  isCompleted
+    ? res("yes the assignment was completed")
+    : rej("the assignment wasnt completed");
+});
 
 completedAssignment
-    .then((data)=>console.log(data))
-    .catch((err)=> console.log(err))
+  .then((data) => console.log(data))
+  .catch((err) => console.log(err));
+
+// Basic Promise Creation: Question: Write a function called simplePromise that creates a new promise which resolves with the value "Promise resolved" after 1 second.
+
+const simplePromise = new Promise((res, rej) => {
+  setTimeout(() => {
+    res("the promise has been resolved after 1 s");
+  }, 1000);
+});
+
+simplePromise
+  .then((data) => console.log(data))
+  .catch((err) => console.log(err))
+  .then(console.log("this is after 1 second"))
 
 
-    
-const promise = new Promise((res,rej)=>{
-    res("this is the success promise data")
-})
 
-promise.then((data)=>{
-    console.log(data)
-}).catch((err)=>{
-    console.log(err)
-})
 
+
+// how does the promise works and its syntax
+const promise = new Promise((res, rej) => {
+  res("this is the success promise data");
+});
+
+promise
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
