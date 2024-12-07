3dfe327025bee86e602c8cc0f2c32aea41bf41af
prakash <p.prakashpaudel35@gmail.com>
2024-12-07 12:31:52 +0530

    structured file

---
+++ day1basics/q5/hoist.js
@@ -0,0 +1,16 @@
+
+
+// console.log(a)
+// var a = "prakash"
+
+// console.log(b)  cannot hoist b cause its block scope
+// let b="paudel"
+
+// console.log(c)
+// const c = "masai"
+
+
+{
+    console.log(a)
+    let a = 0
+}
