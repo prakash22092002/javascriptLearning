3c989a41bd9cb7f507f0d352dba750d65d6cc936
prakash <p.prakashpaudel35@gmail.com>
2024-12-07 23:42:15 +0530

    the promise

--- day2Promises/callBacksPromise/promises.js
+++ day2Promises/callBacksPromise/promises.js
@@ -1,58 +1,40 @@
-
-
-function orderFood(cb){
-    setTimeout(()=>{
-        console.log('ordered food...');
-        cb()
-    },3000)
+function orderFood() {
+    return new Promise((res, rej) => {
+        setTimeout(() => {
+            console.log("ordered food");
+            res("started preparing food");
+            console.log("------------------------------------------------------------------");
+        }, 2000);
+    });
 }
 
-function orderPlaced(cb){
-    setTimeout(()=>{
-        console.log("food order placed...");
-        cb()
-    },1000)
+function prepareFood() {
+    return new Promise((res, rej) => {
+        setTimeout(() => {
+            console.log("food prepared");
+            res("out for delivery, delivery man on the way");
+            console.log("-------------------------------------------------------------------");
+        }, 4000);
+    });
 }
 
-function preparingFood(cb){
-    setTimeout(()=>{
-        console.log("preparing food...");
-        cb()
-    },2000)
+function deliveredFood() {
+    return new Promise((res, rej) => {
+        setTimeout(() => {
+            console.log("food is out for delivery");
+            res("food delivered");
+            console.log("-------------------------------------------------------------------");
+        }, 2000);
+    });
 }
 
-function outTodelivery(cb){
-    setTimeout(()=>{
-        console.log("food prepared and out to delivery...");
-        cb()
-    },5000)
-}
-
-function deliveredFood(){
-    setTimeout(()=>{
-        console.log("enjoy your food...."); 
-    },4000)
-}
-
-// orderFood( ()=>{
-//     orderPlaced( ()=>{
-//         preparingFood( ()=>{
-//             outTodelivery( ()=>{
-//                 deliveredFood()
-//             } )
-//         } )
-//     } )
-// } )
-
-
-// it just makes an call back hell so to over come this we used promises
-
-
-
-
-
-// orderFood()
-// orderPlaced()
-// preparingFood()
-// outTodelivery()
-// deliveredFood(
+// Chaining the Promises
+orderFood()
+    .then(() => prepareFood())
+    .then(() => deliveredFood())
+    .then(() => {
+        console.log("Order process complete");
+    })
+    .catch((error) => {
+        console.error("An error occurred:", error);
+    });
