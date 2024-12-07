327f1556fef4d144dc4f731bbd0a35c951552b9f
prakash <p.prakashpaudel35@gmail.com>
2024-12-08 00:40:44 +0530

    the custome promise learning

--- day2Promises/callBacksPromise/customePromise.js
+++ day2Promises/callBacksPromise/customePromise.js
@@ -1,5 +1,15 @@
 
+const completedAssignment = new Promise((res , rej)=>{
+    const isCompleted = true
+    isCompleted ? res("yes the assignment was completed") : rej("the assignment wasnt completed")
+})
 
+completedAssignment
+    .then((data)=>console.log(data))
+    .catch((err)=> console.log(err))
+
+
+    
 const promise = new Promise((res,rej)=>{
     res("this is the success promise data")
 })
@@ -9,3 +19,4 @@ promise.then((data)=>{
 }).catch((err)=>{
     console.log(err)
 })
+
